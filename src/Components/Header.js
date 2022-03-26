4e35ccc52bedc98cbe392a652f673d330b47b6ca
arogya247 <arogya247@gmail.com>
2022-03-27 03:13:22 +0530

    header component added

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <div style={{padding: '1.5rem', width: '30%', margin: '0 auto'}}>
+      <img src="https://uploads-ssl.webflow.com/617b980e676d90990ab720f5/62209653740c664104b73118_Fountain9_dark_1.svg" 
+      loading="lazy" 
+      alt="" 
+      class="image-14"></img>
+    </div>
+  )
+}
