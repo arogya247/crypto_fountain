3a7afb82f4ccd337f1854ae866569e0cc5b0f72f
arogya247 <arogya247@gmail.com>
2022-03-27 05:41:05 +0530

    search text styled

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -4,8 +4,12 @@ export const Search = ({filter, setFilter}) => {
 
 
   return (
-    <span>
-      Search: {' '}
+    <span style={{display: 'flex', gap: '10px'}}>
+      <div 
+      style={{color: 'white', padding: '.5em', 
+              backgroundColor: '#00ccc2', borderRadius: '5px'}}>
+        Search {' '}
+      </div>
       <input value={filter || ''}
       onChange={e => setFilter(e.target.value)}
       />
